62de55c3cefb643832f848275bed32ec1787fca3
Shixian <shixiangeng@gmail.com>
2021-10-26 20:10:19 +0900

    faster upload

--- site/js/instructions.js
+++ site/js/instructions.js
@@ -2,11 +2,11 @@
 var name = window.location.href.split('=')[1];
 var storageRef = firebase.storage().ref();
 
-for(let i = 1; i < 6; i++){
+// for(let i = 1; i < 6; i++){
 
-	var form = document.getElementById("form"+i.toString());
-	form.addEventListener('submit', uploadVideo(i));
-}
+// 	var form = document.getElementById("form"+i.toString());
+// 	form.addEventListener('submit', uploadVideo(i));
+// }
 // var form = document.getElementById("form");
 // form.addEventListener('submit', uploadVideo());
 var id_indicator = document.getElementById("id_indicator");
@@ -17,12 +17,12 @@ function uploadVideo(index){
 		if(name === ""){
 			alert("あなたの番号を入力してください.");
 		}else{
-			var file = document.getElementById("vid"+index.toString()).files[0];
+			var file = document.getElementById("cameraInput"+(index-1).toString()).files[0];
 			var ref = storageRef.child(name+'/'+index+'-'+Date.now()+'/');
 			var upload = document.getElementById("upload");
 			upload.innerText = "動画" + index.toString()+"アップロード中です...ブラウザを閉じないでください."
 			ref.put(file).then((snapshot) =>{
-			alert("動画" + index.toString()+"アップロード完了.ありがとうございました.");
+			//alert("動画" + index.toString()+"アップロード完了.ありがとうございました.");
 			upload.innerText = "動画" + index.toString()+"アップロード完了. ありがとうございました."
 			});
 			event.preventDefault();
